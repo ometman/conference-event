01fb0a393ee409dda4c7551f64c7a3ed3d0cd65e
ometman <revomet@gmail.com>
2023-04-24 15:53:49 +0000

    Correct linters errors

--- js/main.js
+++ js/main.js
@@ -9,27 +9,26 @@ const mHideShowSpkrs5 = document.getElementById('fifth-spkr');
 const mHideShowSpkrs6 = document.getElementById('sixth-spkr');
 
 mShowMoreSpkrs.addEventListener('click', (e) => {
-mHideShowSpkrs3.style.display = "flex";
-mHideShowSpkrs4.style.display = "flex";
-mHideShowSpkrs5.style.display = "flex";
-mHideShowSpkrs6.style.display = "flex";
+  mHideShowSpkrs3.style.display = 'flex';
+  mHideShowSpkrs4.style.display = 'flex';
+  mHideShowSpkrs5.style.display = 'flex';
+  mHideShowSpkrs6.style.display = 'flex';
 
-mShowMoreSpkrs.style.display = "none"
-mShowLessSpkrs.style.display = "flex"
-    e.preventDefault();
-});
-
-mShowLessSpkrs.addEventListener('click', (e) => {
-  mHideShowSpkrs3.style.display = "none";
-  mHideShowSpkrs4.style.display = "none";
-  mHideShowSpkrs5.style.display = "none";
-  mHideShowSpkrs6.style.display = "none";
-
-  mShowMoreSpkrs.style.display = "flex"
-  mShowLessSpkrs.style.display = "none"
+  mShowMoreSpkrs.style.display = 'none';
+  mShowLessSpkrs.style.display = 'flex';
   e.preventDefault();
 });
 
+mShowLessSpkrs.addEventListener('click', (e) => {
+  mHideShowSpkrs3.style.display = 'none';
+  mHideShowSpkrs4.style.display = 'none';
+  mHideShowSpkrs5.style.display = 'none';
+  mHideShowSpkrs6.style.display = 'none';
+
+  mShowMoreSpkrs.style.display = 'flex';
+  mShowLessSpkrs.style.display = 'none';
+  e.preventDefault();
+});
 
 /* mobile menu ids */
 const mobileMenu = document.getElementsById('main-mobile-menu');
